fc244c479d82c7d2a8009c0b3acaa10948adade0
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2019-03-21 17:25:05 +0000

    Add basic dev setup using webpack
    The setup generates an html file that imports Bulma CSS. `./bin/npm
    start` starts webpack-dev-server accessible from the host on port 8080.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devtool: 'inline-source-map'
+}
