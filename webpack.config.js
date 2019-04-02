02f7c7946b7e0c08b6ba9109850c1e6b9ab1d17f
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2019-04-02 13:18:28 +0100

    Enable CSS extraction
    Extract CSS to main.css in production and use `vue-style-loader` in
    development to enable hot reloading of styles.

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
+const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const webpack = require('webpack');
-
-let config = {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new VueLoaderPlugin()
-  ]
-};
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
+  let config = {
+    module: {
+      rules: [
+        {
+          test: /\.(sc|c)ss$/,
+          use: [
+            argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'main.css',
+        chunkFilename: '[id].css'
+      })
+    ]
+  };
+
   if (argv.mode === 'development') {
     config.devtool = 'inline-source-map';
     config.devServer = { hot: true };
